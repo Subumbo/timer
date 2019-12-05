7aa10839945299a052e530172ebdb49992173e20
Patrick Wolleb <patrick.wolleb.ext@siemens.com>
2019-12-05 07:55:34 +0100

    Add data

---
+++ time.js
@@ -0,0 +1,40 @@
+
+function go(sheet) {
+    console.log(sheet)
+}
+
+var DECEMBER = {
+    "2/12": [{
+        start: 8.1,
+        end: 12
+    },{
+        start: 13,
+        end: 17.2
+    }],
+    "3/12": [{
+        start: 8.1,
+        end: 12
+    },{
+        start: 13,
+        end: 17.45
+    }],
+    "4/12": [{
+        start: 8.4,
+        end: 12
+    },{
+        start: 12.3,
+        end: 17.15
+    },{
+        start: 19.00,
+        end: 20.00
+    }],
+    "5/12": [{
+        start: 8.1,
+        end: 0
+    },{
+        start: 0,
+        end: 0
+    }],
+}
+
+go(DECEMBER)
